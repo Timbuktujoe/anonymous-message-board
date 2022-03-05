ebef7ef5da72c0d5c7c96a9992a26258f0ffd5fc
Timbuktujoe <23214407+Timbuktujoe@users.noreply.github.com>
2022-03-05 14:07:11 +0100

    add POST Route

--- routes/api.js
+++ routes/api.js
@@ -1,32 +1,54 @@
 'use strict';
 
-let mongodb = require('mongodb');
-let mongoose = require('mongoose');
+const BoardModel = require("../models").Board
+const ThreadModel = require("../models").Thread
+const Reply = require("../models").Reply
+
 
 module.exports = function (app) {
-
-  let uri = 'mongodb+srv://timbuktujoe:' + process.env.PW + '@cluster0.78rn7.mongodb.net/anonymous_message_board?retryWrites=true&w=majority'
-  mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
-  let replySchema = new mongoose.Schema({
-    text: { type: String, required: true },
-    delete_password: { type: String, required: true },
-    createdon_: { type: Date, required: true },
-    reported: { type: Boolean, required: true }
-  })
-
-  let threadSchema = new mongoose.Schema({
-    text: { type: String, required: true },
-    delete_password: { type: String, required: true },
-    board: { type: String, required: true },
-    createdon_: { type: Date, required: true },
-    bumpedon_: { type: Date, required: true },
-    reported: { type: Boolean, required: true },
-    replies: { replySchema }
-  })
-
-  app.route('/api/threads/:board');
-
+  app
+    .route("/api/threads/:board")
+    .post((req, res) => {
+      const { text, delete_password } = req.body;
+      let board = req.body.board;
+      if (!board) {
+        board = req.params.board;
+      }
+      console.log("post", req.body);
+      const newThread = new ThreadModel({
+        text: text,
+        delete_password: delete_password,
+        replies: [],
+      });
+      console.log("newThread", newThread);
+      BoardModel.findOne({ name: board }, (err, Boarddata) => {
+        if (!Boarddata) {
+          const newBoard = new BoardModel({
+            name: board,
+            threads: [],
+          });
+          console.log("newBoard", newBoard);
+          newBoard.threads.push(newThread);
+          newBoard.save((err, data) => {
+            console.log("newBoardData", data);
+            if (err || !data) {
+              console.log(err);
+              res.send("There was an error saving in post");
+            } else {
+              res.json(newThread);
+            }
+          });
+        } else {
+          Boarddata.threads.push(newThread);
+          Boarddata.save((err, data) => {
+            if (err || !data) {
+              res.send("There was an error saving in post");
+            } else {
+              res.json(newThread);
+            }
+          });
+        }
+      });
+    })
   app.route('/api/replies/:board');
-
-};
+}
